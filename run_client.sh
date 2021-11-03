599ad331f5cedcf8b99154739ea34ba410cbc0d3
Akis Linardos <linardos.akis@gmail.com>
2021-11-03 13:38:36 +0100

    run_client example

---
+++ run_client.sh
@@ -0,0 +1,1 @@
+python client.py --csv /home/lidia-garrucho/datasets/OPTIMAM/png_screening_cropped_fixed/client_images_screening.csv --dataset /home/lidia-garrucho/datasets/OPTIMAM/png_screening_cropped_fixed/images
